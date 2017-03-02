f8a4c33ff7b8b4716723dc3bbacb0815023093b9
Sergii Bosovskyi <bosovskyi@gmail.com>
2017-03-02 11:59:20 +0200

    changes to annotation processor. some tools for androidTest

---
+++ app/src/androidTest/java/com/bosovskyi/showstime/FragmentTestRule.java
@@ -0,0 +1,47 @@
+package com.bosovskyi.showstime;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by boss1088 on 3/1/17.
+ */
+public class FragmentTestRule<F extends Fragment> extends ActivityTestRule<TestActivity> {
+
+    private final Class<F> mFragmentClass;
+    private F mFragment;
+
+    public FragmentTestRule(final Class<F> fragmentClass) {
+        super(TestActivity.class, true, false);
+        mFragmentClass = fragmentClass;
+    }
+
+    @Override
+    protected void afterActivityLaunched() {
+        super.afterActivityLaunched();
+
+        getActivity().runOnUiThread(() -> {
+            try {
+                //Instantiate and insert the fragment into the container layout
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                mFragment = mFragmentClass.newInstance();
+                transaction.add(R.id.contentFrame, mFragment);
+                transaction.commit();
+            } catch (Exception e) {
+                Assert.fail(String.format("%s: Could not insert %s into TestActivity: %s",
+                        getClass().getSimpleName(),
+                        mFragmentClass.getSimpleName(),
+                        e.getMessage()));
+            }
+        });
+    }
+    public F getFragment(){
+        return mFragment;
+    }
+}
---
+++ app/src/androidTest/java/com/bosovskyi/showstime/MockApp.java
@@ -0,0 +1,15 @@
+package com.bosovskyi.showstime;
+
+import com.bosovskyi.showstime.di.components.AppComponent;
+
+/**
+ * Created by boss1088 on 3/2/17.
+ */
+
+public class MockApp extends App {
+
+    @Override
+    protected AppComponent buildComponent() {
+        return super.buildComponent();
+    }
+}
